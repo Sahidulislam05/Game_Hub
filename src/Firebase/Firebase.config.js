cbe762845afe08dd3caaf1478c088a9e9a0fee84
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-10-23 20:42:52 +0600

    Added Game Hub Home

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeO6GeSiN4ypxESq-Id6sC1e-j3daOEYU",
-  authDomain: "game-hub-a7422.firebaseapp.com",
-  projectId: "game-hub-a7422",
-  storageBucket: "game-hub-a7422.firebasestorage.app",
-  messagingSenderId: "377227533555",
-  appId: "1:377227533555:web:74d3899f0ac9ec91f4ab86",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
