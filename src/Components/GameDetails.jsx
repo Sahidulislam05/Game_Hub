537d64fbb75ab18946e822bc7993a0fd0bf8c687
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-10-24 11:57:07 +0600

    Added Registration Page

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -9,7 +9,16 @@ const GameDetails = () => {
   // console.log(apps);
   const games = apps.find((game) => String(game.id) === id);
   // console.log(games);
-  const { description, title, ratings, developer, coverPhoto } = games || {};
+  const {
+    description,
+    title,
+    ratings,
+    developer,
+    coverPhoto,
+    category,
+    releaseYear,
+    downloadLink,
+  } = games || {};
   return (
     <div>
       <title> Game Details </title>
@@ -17,14 +26,28 @@ const GameDetails = () => {
         <div className="card bg-base-100 w-[70%] shadow-sm">
           <figure className="mt-10">
             <img src={coverPhoto} alt="Shoes" />
-            
           </figure>
           <div className="card-body">
             <h2 className="card-title">{title}</h2>
+            <h2 className="font-semibold">Category: {category}</h2>
+            <p> Release Year: {releaseYear}</p>
             <p>{description}</p>
+            <h1>
+              Download Link :
+              <a
+                href={downloadLink}
+                target="_blank"
+                className="underline cursor-pointer text-red-400 ml-2"
+              >
+                {downloadLink}
+              </a>
+            </h1>
+
             <div className="card-actions justify-end">
-              <div className="badge badge-outline">{ratings}</div>
-              <div className="badge badge-outline">{developer}</div>
+              <div className="badge badge-outline">Rating: {ratings}</div>
+              <div className="badge badge-outline">
+                Developed by: {developer}
+              </div>
             </div>
           </div>
         </div>
